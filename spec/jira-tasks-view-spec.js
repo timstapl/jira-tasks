2c85e56ea263d1d7ff8a96ba10bb7ebfae761e79
Timothy Stapleton <retrocede@outlook.com>
2017-04-08 15:09:54 -0700

    initial commit (atom package gen)

---
+++ spec/jira-tasks-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import JiraTasksView from '../lib/jira-tasks-view';
+
+describe('JiraTasksView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
