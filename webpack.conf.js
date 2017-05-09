38d168d6a075b2c12331aa53a105ebf8fa3603cc
Timothy Stapleton <retrocede@outlook.com>
2017-05-09 08:36:39 -0700

    Removed ignoring node_modules to see if that fixes #5

--- webpack.conf.js
+++ webpack.conf.js
@@ -27,7 +27,7 @@ module.exports = {
                 test: /\.js$/,
                 loader: 'babel-loader',
                 include: projectRoot,
-                exclude: /node_modules/,
+                // exclude: /node_modules/,
             },
             {
                 test: /\.vue$/,
